0d6f7fec9c69d6110f080b1892f643b17e716d6c
1234-yyy <1505192241@qq.com>
2025-08-17 18:57:40 +0800

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,1 +1,1 @@
-loadstring(游戏：HttpGet("https://raw.githubusercontent.com/1234-yyy/roblox-scripts/main/script.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/1234-yyy/roblox-scripts/main/script.lua"))()
